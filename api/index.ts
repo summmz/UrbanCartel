f935a532d848762cfe8dddb332bd1c6697fb0dd5
summmz <isumit7869@gmail.com>
2026-04-05 13:37:52 +0530

    fix: Vercel serverless API

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,25 @@
-import { app } from "../server/_core/index";
+import "dotenv/config";
+import express from "express";
+import { createExpressMiddleware } from "@trpc/server/adapters/express";
+import { registerOAuthRoutes } from "../server/_core/oauth";
+import { registerLocalAuthRoutes } from "../server/_core/localAuth";
+import { appRouter } from "../server/routers";
+import { createContext } from "../server/_core/context";
+
+const app = express();
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
+registerOAuthRoutes(app);
+registerLocalAuthRoutes(app);
+
+app.use(
+  "/api/trpc",
+  createExpressMiddleware({
+    router: appRouter,
+    createContext,
+  })
+);
 
 export default app;
